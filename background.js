17b4c3ab263711b6d65598eeebc08ba241034a79
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2021-04-27 01:41:09 +0200

    Add files via upload
    Radio Maroc FM/AM. Écouter radio Marocaine en ligne |

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.browserAction.onClicked.addListener(function() {
+	chrome.tabs.query({
+		currentWindow: true,
+		active: true
+	}, function(tab) {
+		chrome.tabs.create({
+					"url": "RADIO.html"
+
+		});
+	});
+});
