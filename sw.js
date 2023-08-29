a5a3b15e37d6e72bd7ed4eee6fc06230218267ed
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-08-29 03:32:24 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('app-store').then((cache) => cache.addAll([
+      '/images/WhatsApp.png',
+      '/images/logohead.png',
+      '/images/icons-192.png',
+      '/images/icons-512.png',
+      '/icon/app-icon_192x192.png',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
