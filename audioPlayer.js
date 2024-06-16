dc0b83335acca357ae804d9459b19f9901f4b5f6
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2024-06-16 01:17:31 -0400

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,200 +1,200 @@
-/*
-    Default constructor configuration:
-        autoplay: false,
-        shuffle: false,
-        loop: false,
-        playerId: "audioPlayer",
-        playlistId: "playlist",
-        currentClass: "current-song"
-        
-    Methods:
-        setLoop
-        setShuffle
-        toggleShuffle
-        toggleLoop
-        prevTrack
-        nextTrack
-    
-    Can access player by .player variable
-    example playlist.player.pause();
-*/
-/*------------------------------------------------*/
-// Press spacebar to Play/Pause.
-var x = document.getElementsByClassName("oui-image-cover");
-var audio = document.getElementById('audioPlayer');
+document.addEventListener('DOMContentLoaded', function () {
+    // Configuration
+    const config = {
+        autoplay: true,
+        shuffle: true,
+        volume: 0.9,
+        autoPlay: false,
+        preload: "auto",
+        randomPlay: false,
+        loopingOn: true,
+        mediaEndAction: "next",
+        usePlaylistScroll: true,
+        playlistScrollOrientation: "vertical",
+        playlistScrollTheme: "light-thin",
+        useKeyboardNavigationForPlayback: true,
+        createDownloadIconsInPlaylist: true,
+        createLinkIconsInPlaylist: true,
+        facebookAppId: "",
+        useNumbersInPlaylist: false,
+        numberTitleSeparator: ".  ",
+        artistTitleSeparator: " - ",
+        sortableTracks: false,
+        playlistItemContent: "title",
+        useMediaSession: true,
+        useStatistics: false,
+        autoOpenPopup: false
+    };
 
+    // Elements
+    const video = document.getElementById('videoPlayer');
+    const playPauseBtn = document.getElementById('play-pause-button');
+    const nextBtn = document.getElementById('next-button');
+    const prevBtn = document.getElementById('prev-button');
+    const x = document.getElementsByClassName("oui-image-cover");
 
-
-if (audio) {
-  window.addEventListener('keydown', function (event) {
-    var key = event.which || event.keyCode;
-	var btn = $(".button");
-
-    if (key === 32) { // spacebar
-      // eat the spacebar, so it does not scroll the page
-	       togglePlayPause();
-	   event.preventDefault();
-      audio.paused ? audio.play() : audio.pause();
-	      }
-  });
-}
-
-/*
-$(document).ready(function() {
-var btn = $(".button");
-btn.click(function() {
-btn.toggleClass("paused");
-    return false;
-  });
-});
-*/
-
-function togglePlayPause() {
-	   mediaPlayer = document.getElementById('audioPlayer');
-   var btn = document.getElementById('play-pause-button');
-   if (! mediaPlayer.paused) {
-      btn.title = 'play';
-      btn.className = 'play';
-      audio.poster = x[this.trackPos].src;
-
-   }
-   else {
-      btn.title = 'pause';
-      btn.className = 'pause';
-      audio.poster = x[this.trackPos].src;
-   }
-}
-
-	/*------------------------------------------------------------*/
-
-class AudioPlaylist{
-    randomizeOrder(){
-        for (var i = this.trackOrder.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = this.trackOrder[i];
-            this.trackOrder[i] = this.trackOrder[j];
-            this.trackOrder[j] = temp;
-        }
-        return this.trackOrder;
+    // Keyboard controls
+    if (video) {
+        window.addEventListener('keydown', function (event) {
+            if (event.key === ' ') { // spacebar
+                event.preventDefault();
+                togglePlayPause();
+                video.paused ? video.play() : video.pause();
+            }
+        });
     }
-	
-    setTrack(arrayPos) {
-        const liPos = this.trackOrder[arrayPos];
-        const trackUrl = document.querySelector(`#${this.playlistId} li:nth-child(${liPos + 1}) a`).href;
-        this.player.src = trackUrl;
 
-        if (Hls.isSupported() && trackUrl.endsWith('.m3u8')) {
+    // Class for video playlist
+    class VideoPlaylist {
+        constructor(config = {}) {
+            this.shuffle = config.shuffle;
+            this.playerId = config.playerId || "videoPlayer";
+            this.playlistId = config.playlistId || "playlist";
+            this.currentClass = config.currentClass || "current-video";
+            this.length = document.querySelectorAll(`#${this.playlistId} li`).length;
+            this.player = document.getElementById(this.playerId);
+            this.autoplay = config.autoplay || this.player.autoplay;
+            this.loop = config.loop;
+            this.trackPos = 0;
+            this.trackOrder = Array.from({ length: this.length }, (_, i) => i);
+
+            this.init();
+        }
+
+        init() {
+            document.querySelectorAll(`#${this.playlistId} li a`).forEach((link, index) => {
+                link.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    this.setTrack(this.trackOrder.indexOf(index));
+                    this.player.play();
+                    this.updateUI();
+                });
+            });
+
+            if ('mediaSession' in navigator) {
+                navigator.mediaSession.setActionHandler('previoustrack', () => this.prevTrack());
+                navigator.mediaSession.setActionHandler('nexttrack', () => this.nextTrack());
+            }
+
+            playPauseBtn.addEventListener('click', () => this.playPause());
+            prevBtn.addEventListener('click', () => this.prevTrack());
+            nextBtn.addEventListener('click', () => this.nextTrack());
+        }
+
+        randomizeOrder() {
+            for (let i = this.trackOrder.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [this.trackOrder[i], this.trackOrder[j]] = [this.trackOrder[j], this.trackOrder[i]];
+            }
+        }
+
+        setTrack(arrayPos) {
+            const liPos = this.trackOrder[arrayPos];
+            const trackUrl = document.querySelector(`#${this.playlistId} li:nth-child(${liPos + 1}) a`).href;
+
             if (this.hls) {
                 this.hls.destroy();
+                this.hls = null;
             }
-            this.hls = new Hls();
-            this.hls.loadSource(trackUrl);
-            this.hls.attachMedia(this.player);
-        } else if (this.player.canPlayType('application/vnd.apple.mpegurl')) {
-            this.player.src = trackUrl;
+
+            const knownHlsUrls = [
+                'https://stream.bodkas.com/playlist?id=mfmradio',
+                'https://stream.bodkas.com/playlist?id=3',
+                'https://stream.bodkas.com/playlist?id=chadafmradio',
+                'http://stream3.broadcast-associes.com:8405/Radio-Orient',
+                'https://izlan.fr/radios/atlas/stream',
+                'http://stream1.coran.tk:8005/stream/1/',
+                'http://channel0.moroccanvoice.com:8000/;stream/1',
+                'https://mbn-channel-04.akacast.akamaistream.net/7/26/233453/v1/ibb.akacast.akamaistream.net/mbn_channel_04',
+                'https://stream2.atlanticradio.ma:9300/stream',
+                'https://manager8.streamradio.fr:1775/stream',
+                'https://listen.radioking.com/radio/252934/stream/297385'
+            ];
+
+            const hlsMimeTypes = [
+                'application/vnd.apple.mpegurl',
+                'application/x-mpegURL',
+                'application/vnd.apple.mpegurl.audio',
+                'application/vnd.apple.mpegurl.video'
+            ];
+
+            const fileType = trackUrl.split('.').pop().toLowerCase();
+            const fileHash = trackUrl.split('#').pop().toLowerCase();
+            const isKnownHlsUrl = knownHlsUrls.includes(trackUrl);
+
+            this.player.src = '';
+
+            if (Hls.isSupported() && (fileType === 'm3u8' || fileType === 'm3u' || isKnownHlsUrl)) {
+                this.hls = new Hls();
+                this.hls.loadSource(trackUrl);
+                this.hls.attachMedia(this.player);
+            } else if (fileType === 'mp3' || fileType === 'ogg' || fileType === 'aac' || fileHash === "aud") {
+                this.player.src = trackUrl;
+            } else if (hlsMimeTypes.some(type => this.player.canPlayType(type) !== '')) {
+                this.player.src = trackUrl;
+            } else {
+                console.error('Unsupported media type');
+                return;
+            }
+
+            this.player.play().catch(error => {
+                console.error('Error playing the media:', error);
+            });
+
+            document.querySelector(`.${this.currentClass}`).classList.remove(this.currentClass);
+            document.querySelector(`#${this.playlistId} li:nth-child(${liPos + 1})`).classList.add(this.currentClass);
+            this.trackPos = arrayPos;
+            this.updateUI();
+        }
+
+        playPause() {
+            togglePlayPause();
+            event.preventDefault();
+            video.paused ? video.play() : video.pause();
+        }
+
+        prevTrack() {
+            this.trackPos === 0 ? this.setTrack(0) : this.setTrack(this.trackPos - 1);
+            this.player.play();
+            playPauseBtn.src = './image/pause.png';
+            this.updateUI();
+        }
+
+        nextTrack() {
+            if (this.trackPos < this.length - 1) {
+                this.setTrack(this.trackPos + 1);
+            } else {
+                if (this.shuffle) this.randomizeOrder();
+                this.setTrack(0);
+            }
+            this.player.play();
+            playPauseBtn.src = './image/pause.png';
+            this.updateUI();
+        }
+
+        updateUI() {
+            document.title = x[this.trackPos].title;
+            document.getElementById("demo").innerHTML = x[this.trackPos].title;
+            video.poster = x[this.trackPos].src;
+        }
+    }
+
+    const playlist = new VideoPlaylist(config);
+
+    // Prevent links from opening in a new tab
+    document.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A' && event.target.closest(`#${playlist.playlistId}`)) {
+            event.preventDefault();
+        }
+    });
+
+    function togglePlayPause() {
+        if (!video.paused) {
+            playPauseBtn.src = './image/play.png';
+            video.poster = x[playlist.trackPos].src;
         } else {
-            this.player.src = trackUrl;
+            playPauseBtn.src = './image/pause.png';
+            video.poster = x[playlist.trackPos].src;
         }
-
-        document.querySelector(`.${this.currentClass}`).classList.remove(this.currentClass);
-        document.querySelector(`#${this.playlistId} li:nth-child(${liPos + 1})`).classList.add(this.currentClass);
-        this.trackPos = arrayPos;
-        document.title = x[this.trackPos].title;
-        document.getElementById("demo").innerHTML = x[this.trackPos].title;
-        audio.poster = x[this.trackPos].src;
-        togglePlayPause();
     }
-	
-
-playpause(){
-	   togglePlayPause();
-	   event.preventDefault();
-      audio.paused ? audio.play() : audio.pause();
-	  document.title = x[this.trackPos].title;
-      audio.poster = x[this.trackPos].src;
-
-
-
-}
-
-prevTrack(){
-		var btn = document.getElementById('play-pause-button');
-		    var btn = $(".button");
-
-        if(this.trackPos == 0)
-            this.setTrack(0);
-        else
-            this.setTrack(this.trackPos - 1);
-        this.player.play(); 
-      btn.className = 'pause';
-	  	    //togglePlayPause();
-	document.title = x[this.trackPos].title;
-	document.getElementById("demo").innerHTML = x[this.trackPos].title;
-    audio.poster = x[this.trackPos].src;
-
-    }
-	
-nextTrack(){
-var btn = document.getElementById('play-pause-button');
-var btn = $(".button");
-// if track isn't the last track in array of tracks, go to next track
-if(this.trackPos < this.length - 1)
-this.setTrack(this.trackPos+1);
-else{
-if(this.shuffle)
-this.randomizeOrder();
-this.setTrack(0);
-}
-this.player.play();			
-btn.className = 'pause';
-//togglePlayPause();
-document.title = x[this.trackPos].title;
-document.getElementById("demo").innerHTML = x[this.trackPos].title;
-audio.poster = x[this.trackPos].src;
-}
-
-	
-
-constructor(config = {} ){
-        
-        /***
-        *
-        *       setting defaults, and initialzing player 
-        *
-        */
-		var x = document.getElementsByClassName("oui-image-cover");
-		var btn = $(".button");
-		var btn = document.getElementById('play-pause-button');
-        var classObj = this; // store scope for event listeners
-        this.shuffle = (config.shuffle === true) ? true : false;
-        this.playerId = (config.playerId) ? config.playerId : "audioPlayer";
-        this.playlistId = (config.playlistId) ? config.playlistId : "playlist";
-        this.currentClass = (config.currentClass) ? config.currentClass : "current-song"
-        this.length = $("#"+this.playlistId+" li").length; 
-        this.player = $("#"+this.playerId)[0];
-        this.autoplay = (config.autoplay === true || this.player.autoplay) ? true : false;
-        this.loop = (config.loop === true) ? true : false;
-        this.trackPos = 0;
-        this.trackOrder = [];
-        for(var i = 0; i < this.length; i++){
-            this.trackOrder.push(i);
-        }
-
-	
-        /*       handle link click   */
-	
-        $("#"+this.playlistId+" li a ").click(function(e){
-            e.preventDefault();
-            // set track based on index of 
-            classObj.setTrack(classObj.trackOrder.indexOf($(this).parent().index()));
-            classObj.player.play();
-document.title = x[this.trackPos].title;
-document.getElementById("demo").innerHTML = x[this.trackPos].title;
-audio.poster = x[this.trackPos].src;
-        });
-  
-    }
-	// stalled
-	// stalled
-	
-}
+});
